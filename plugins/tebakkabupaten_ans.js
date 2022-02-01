8b6157f83b1962a3160881de25cc50d5762a787b
۩๖ۣۜℜLƤ27 <85769960+Laksmana27@users.noreply.github.com>
2022-02-01 21:15:44 +0700

    Update tebakkabupaten_ans.js

--- plugins/tebakkabupaten_ans.js
+++ plugins/tebakkabupaten_ans.js
@@ -3,7 +3,7 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*lalu/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*laly/i.test(m.quoted.contentText)) return !0
     this.tebakkabupaten = this.tebakkabupaten ? this.tebakkabupaten : {}
     if (!(id in this.tebakkabupaten)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.tebakkabupaten[id][0].id) {
